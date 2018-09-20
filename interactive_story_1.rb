96ff89db245bb18683953b15f35650ab7794d733
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-09-20 11:58:35 +0100

    Added options for full story and methods for trading

--- interactive_story_1.rb
+++ interactive_story_1.rb
@@ -7,7 +7,7 @@ bag: "small"
 
 @which_option = {forest: @options_forest = {"Check inventory" => {text: "Your clothes are in good condition and will keep you warm. You have a small bag and an empty water bottle.", location: "forest"},
 "Check berry bushes" => {text: "You move closer to the berry bushes and see they are covered in bright red berries. They look tastey.", location: "berries"},
-"Move north" => {text: "You move towards the sound of the water and find a clear stream cutting through the tree line. Across the stream, you can see wide open fields. Upstream, you see a mountain area, and downstream you see that the water continues flowing through the trees.", location: "river1"},
+"Move north" => {text: "You move towards the sound of the water and find a clear stream cutting through the tree line. Across the stream, you can see wide open fields. Upstream, you see a mountain area, and downstream you see that the water continues flowing through the trees, pas some caves.", location: "river1"},
 "Move east" => {text: "You attempt to head east but the trees soon become too thick. You head back to the clearing.", location: "forest"},
 "Move south" => {text: "You attempt to head south but the trees soon become too thick. You head back to the clearing.", location: "forest"},
 "Move west" => {text: "You attempt to head west but the trees soon become too thick. You head back to the clearing.", location: "forest"}
@@ -48,7 +48,7 @@ fields: @options_fields = {"Check inventory" => {text: "Your clothes are in good
 track: @options_track = {"Check inventory" => {text: "Your clothes are in good condition and will keep you warm. You have a small bag and an empty water bottle.", location: "track"},
 "Go left" => {text: "Heading left, you walk for what feels like hours, watching the sun slowly sink towards the horizon. You're really thirsty.", location: "track_left_water"},
 "Go right" => {text: "Heading right, you walk for what feels like hours, watching the sun slowly sink towards the horizon. You're really thirsty.", location: "track_right_water"},
-"Look at hoof-prints" => {text: "There are a hoof-prints covering almost every inch of the dirt track. The most recent ones appear to be facing left.", location: "track"}
+"Look at hoof-prints" => {text: "There are hoof-prints covering almost every inch of the dirt track. The most recent ones appear to be facing left.", location: "track"}
 },
 track_left_water: @options_track_left_water = {"Check water bottle" => {text: "You check your water bottle", location: "track_left_water"}
 },
@@ -56,6 +56,12 @@ track_right_water: @options_track_right_water = {"Check water bottle" => {text: 
 },
 track_left: @options_track_left = {"Check inventory" => {text: "N/A", location: "track_left"},
 "Look around" => {text: "Looking around, you notice a cart filled with goods. It appears to be unoccupied", location: "cart"}
+},
+cart: @options_cart = {"Take food" => {text: "An angry merchant returns with his horse and shoots you for being a thief.
+  GAME OVER", location: "end"},
+"Wait for merchant" => {text: "A merchant returns with his horse and finds you waiting by his cart. He quickly checks his goods and is pleased to find you haven't taken anything. He offers you a ride to town in exchange for something he can trade.", location: "trade"}
+},
+trade: @options_trade = {"Check for item to trade" => {text: "You check your inventory", location: "trade"}
 },
 track_right: @options_track_right = {"Check inventory" => {text: "N/A", location: "track_right"},
 "Look around" => {text: "Looking around, you realise you are completely lost. The grass of the fields is now nearly as tall as you and you can't see far ahead or behind. You turn around and head back.", location: "track"}
@@ -77,7 +83,7 @@ outside_cave3: @options_outside_cave3 = {"Check inventory" => {text: "Your cloth
 "Head back to fast stream" => {text: "You head back towards the stream near where you awoke.", location: "river1"}
 },
 cave1: @options_cave1 = {"Turn around and exit cave" => {text: "Unable to find your way, your turn around and exit the cave", location: "outside_cave1"},
-"Keep moving forwards" => {text: "You head further into the cave, your eyes adjusting slightly to the darkness. You reach a fork in the path.", location: "cave_fork_water"}
+"Keep moving forwards" => {text: "You head further into the cave, your eyes adjusting slightly to the darkness. You reach a fork in the path. You're really thirsty and feel as though you could pass out.", location: "cave_fork_water"}
 },
 cave_fork_water: @options_cave_fork_water = {"Check water bottle" => {text: "You check your water bottle", location: "track_left_water"}
 },
@@ -94,21 +100,20 @@ farm: @options_farm = {"Check inventory" => {text: "Your clothes are in good con
 farm_house: @options_farm_house = {"Check inventory" => {text: "Your clothes are in good condition and will keep you warm. You have a small bag and an empty water bottle.", location: "farm_house"},
 "Knock on door" => {text: "You timidly knock on the door, tired after your adventure. No one answers.", location: "farm_house2"},
 "Enter house without knocking" => {text: "An angry farmer rushes at you, kicking you out of his house. You try to explain your situation but he refuses to let you in. With nowhere to shelter over night, you perish to the cold.
-  GAME OVER", location: "end"},
+  GAME OVER", location: "end"}
+},
 farm_house2: @options_farm_house2 = {"Knock again" => {text: "You knock again, slightly louder. An elderly woman opens the door and invites you in. You rest overnight and are able to get home the next morning.
   YOU WIN", location: "end"},
 "Enter house" => {text: "An angry farmer rushes at you, kicking you out of his house. You try to explain your situation but he refuses to let you in. With nowhere to shelter over night, you perish to the cold.
   GAME OVER", location: "end"}
 }
 }
-}
 
 puts "As the sharp chirp of birds infiltrates your slumber, you groggily open your eyes.
-Instead of the soft light of the morning sun filtering through your curtains revealing
-the familiar sight of your bedroom, you find you're in a small clearing of a dense forest.
-Standing up, you take in your surroundings. You can see some dark green bushes littered
-with crimson berries dotted between the trees. You can hear the sound of running water
-to the north."
+Soft morning light filters through leaves above you, and you find you're in a small
+clearing of a dense forest.Standing up, you take in your surroundings. You can see some
+dark green bushes littered with crimson berries dotted between the trees. You can hear
+the sound of running water to the north."
 
 def player_choice
   puts "..
@@ -121,10 +126,10 @@ What would you like to do?"
       end
     end
   end
-  @action = gets.chomp
+  @action = gets.chomp.capitalize
   while !@all_options.include?(@action)
     puts "Please type the command exactly as it shows."
-    @action = gets.chomp
+    @action = gets.chomp.capitalize
   end
 end
 
@@ -135,6 +140,8 @@ def action_output
     end
   elsif @thirst.include?(@location)
     thirsty
+  elsif @action == "Check for item to trade"
+    trade
   else
   puts @which_option[@location.to_sym][@action][:text]
   @location = @which_option[@location.to_sym][@action][:location]
@@ -149,8 +156,6 @@ def update_inventory
     if @location == "waterfall1"
       @inventory[:fish] = 1
     end
-  when "Offer fish"
-    @inventory[:fish] = 0
   when "Try to cross river"
     @inventory[:clothes] = "wet"
   end
@@ -167,8 +172,20 @@ def thirsty
     @location = "end"
   else
     puts "Thankful that you thought to fill your water bottle up, you take it out and drink it."
-    @inventory[:water_bottle] = "empty"
     @location = @location.chomp!("_water")
+  end
+end
+
+def trade
+  if @inventory[:fish] == 1
+    puts "You offer up the fish you caught earlier."
+    puts "Satisfied, the merchant gives you a ride to the nearest village. You're safe.
+  YOU WIN"
+    @location = "end"
+  else
+    puts "You have nothing to trade. The merchant leaves and you are stranded as night draws in. Your clothes are wet from crossing the river and offer no warmth as cold permeates the air. You fall victim to hypothermia and die.
+    GAME OVER"
+    @location = "end"
   end
 end
 
